b31ce81395a9cca97c36cb294926bd8268b1fc05
f4j4rmaulana <f4j4rmaulana@gmail.com>
2023-12-02 08:42:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kamar-edukasi.dev
---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'e7tXgQJIu6kJuT60qSLHThNJUqtN9o6Rlocalhost';
+$CFG->bootstraphash = 'ac142ced3abeeeccecbb60e3e6800cc2';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
